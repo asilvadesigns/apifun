0ed6793aa6a606243865a0b9b24fcc0d30d97717
Alberto Silva <asilvadesigns@gmail.com>
2017-10-18 22:55:29 -0400

    early data persistence

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
+const CONFIG = require('./config');
+const DATA   = require('./measurements.json');
+
 const fs = require('fs');
-const express = require('express');
-const app = express();
+const _  = require('lodash');
+
+const express    = require('express');
+const app        = express();
 const bodyParser = require('body-parser');
-//let data = fs.readFileSync('./measurements.json');
-//let measurements = JSON.parse(data);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'hello world!' });
-})
+});
 
-//app.get('/measurements', (req, res) => {
-  //res.json(measurements);
-//})
+app.get('/measurements', (req, res) => {
+  res.json(DATA);
+});
 
 app.post('/measurements', (req, res) => {
   if (!req.body.timestamp) {
     return res.status(400).end();
   }
 
-  let POST_DATA = JSON.stringify(req.body, null, 2);
-  fs.writeFile('./measurements.json', POST_DATA, (err) => { console.log(err); });
+  DATA.measurements.push(req.body);
+  fs.writeFile('./measurements.json', JSON.stringify(DATA, null, 2), err => {
+    if (err) console.log(err);
+  });
+
   res.type('application/json');
   res.location(req.url + req.body.timestamp);
   res.status(201).end();
-})
+});
 
 app.listen(3000, _ => {
   console.log('Server listenting on port ' + 3000 + ' ...');
-})
+});
