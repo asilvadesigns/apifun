e18432bf928b4cf571589a8465d7add6a0f13cc9
Alberto Silva <asilvadesigns@gmail.com>
2017-10-23 12:31:13 -0400

    how not to validate

--- app/routes/measurements.js
+++ app/routes/measurements.js
@@ -5,6 +5,21 @@ measurements.get('/', (req, res) => {
 });
 
 measurements.post('/', (req, res) => {
+
+  if (typeof(req.body.timestamp) !== "string") {
+    return res.status(400).json({
+      message: 'timestamp must be a string',
+      posting: req.body.timestamp
+    });
+  }
+
+  if (typeof(req.body.temperature) !== "number") {
+    return res.status(400).json({
+      message: 'temperature must be a number',
+      posting: req.body.temperature
+    });
+  }
+
   res.status(201).json({
     message: 'attempting to post here...',
     posting: req.body
