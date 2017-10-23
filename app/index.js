45e4880c7cb0949c2f6b3d92c4dbb9f58ee79127
Alberto Silva <asilvadesigns@gmail.com>
2017-10-23 12:13:22 -0400

    parse json bodies, early post route

--- app/index.js
+++ app/index.js
@@ -2,7 +2,9 @@ const CONFIG = require("./config");
 const ROUTES = require("./routes");
 
 const app = require("express")();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 app.use('/', ROUTES);
 
 app.listen(CONFIG.PORT, () => {
--- app/routes/measurements.js
+++ app/routes/measurements.js
@@ -4,4 +4,11 @@ measurements.get('/', (req, res) => {
   res.status(200).json({ message: 'no measurements yet!' });
 });
 
+measurements.post('/', (req, res) => {
+  res.status(201).json({
+    message: 'attempting to post here...',
+    posting: req.body
+  });
+});
+
 module.exports = measurements;
