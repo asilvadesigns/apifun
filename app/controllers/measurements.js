722c6fad2a7cf42627fdf3809f132d9ec3643801
Alberto Silva <asilvadesigns@gmail.com>
2017-10-24 22:31:03 -0400

    introduced logic for GETing dates or date time measurements

--- app/controllers/measurements.js
+++ app/controllers/measurements.js
@@ -17,9 +17,52 @@ measurements.get("/", (req, res) => {
 });
 
 measurements.get("/:timestamp", (req, res) => {
+
+  let date     = moment(req.params.timestamp, 'YYYY-MM-DD', true);
+  let datetime = moment(req.params.timestamp, 'YYYY-MM-DDTHH:mm:ss.sssZ', true);
+  let request  = req.params.timestamp;
+  let query;
+
+  if (datetime.isValid()) {
+    query = store.measurements.filter((measurement) => {
+      if (measurement.timestamp === request) return measurement;
+    });
+  } else if (date.isValid()) {
+    query = store.measurements.filter((measurement) => {
+      if (measurement.timestamp.includes(request)) return measurement;
+    });
+  } else {
+    return res.status(400).json({
+      heading: "bad request...",
+      message: request
+    });
+  }
+
+  if (!query || query.length === 0) {
+    return res.status(404).json({
+      heading: "timestamp not found...",
+      message: request
+    });
+  } else {
+    return res.status(200).json({
+      heading: "measurement found!",
+      message: query
+    });
+  }
+});
+
+measurements.get("/:date", (req, res) => {
+
+  let date = moment(req.params.date, 'YYYY-MM-DD', true);
+  if (!date.isValid()) {
+    return res.status(404).json({
+      heading: "date is not valid...",
+      message: req.params.date
+    });
+  }
+
   let query = store.measurements.filter((measurement) => {
-    var date = moment(req.params.timestamp, 'YYYY-MM-DD', true);
-    if (date.isValid() || measurement.timestamp === req.params.timestamp) {
+    if (measurement.timestamp.includes(date)) {
       return measurement;
     }
   });
@@ -27,7 +70,7 @@ measurements.get("/:timestamp", (req, res) => {
   if (!query || query.length === 0) {
     return res.status(404).json({
       heading: "timestamp not found...",
-      message: req.params.timestamp
+      message: req.params.date
     });
   }
 
