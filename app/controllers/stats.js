da63aabce8fc9c89322985b6e5243df51188cfa4
Alberto Silva <asilvadesigns@gmail.com>
2017-10-25 17:04:58 -0400

    stat querysting returns metric

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -6,11 +6,25 @@ const model = require("../models/measurements.js");
 const store = require("../store");
 
 stats.get("/", (req, res) => {
+
+  let param  = req.query.param;
+  let stat   = req.query.stat;
+  let metric = req.query.metric;
+  let from   = req.query.fromDateTime;
+  let to     = req.query.toDateTime;
+
+  let dbquery;
+  dbquery = store.measurements.filter((measurement) => {
+    if (measurement.hasOwnProperty(metric)) return measurement;
+  });
+
   res.status(200).json({
     heading: "querystring test...",
-    message: req.query
+    message: {
+      querystring: req.query,
+      queryresult: dbquery
+    }
   })
 });
 
 module.exports = stats;
-
--- app/store/index.js
+++ app/store/index.js
@@ -1,5 +1,34 @@
 const store = {
-  "measurements": []
-}
+  measurements: [
+    {
+      timestamp: "2015-09-01T16:00:00.000Z",
+      temperature: 27.1,
+      dewPoint: 16.9
+    },
+    {
+      timestamp: "2015-09-01T16:10:00.000Z",
+      temperature: 27.3
+    },
+    {
+      timestamp: "2015-09-01T16:20:00.000Z",
+      temperature: 27.5,
+      dewPoint: 17.1
+    },
+    {
+      timestamp: "2015-09-01T16:30:00.000Z",
+      temperature: 27.4,
+      dewPoint: 17.3
+    },
+    {
+      timestamp: "2015-09-01T16:40:00.000Z",
+      temperature: 27.2
+    },
+    {
+      timestamp: "2015-09-01T17:00:00.000Z",
+      temperature: 28.1,
+      dewPoint: 18
+    }
+  ]
+};
 
 module.exports = store;
