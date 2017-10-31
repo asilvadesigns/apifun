c072320ebf7c855b65834b3b9df76a84944be3ff
Alberto Silva <asilvadesigns@gmail.com>
2017-10-31 16:17:24 -0400

    ocd indentations

--- app/index.js
+++ app/index.js
@@ -1,16 +1,14 @@
 const CONFIG = require("./config");
 const ROUTES = require("./routes");
 
-const express = require("express");
+const express    = require("express");
 const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const validator = require("express-validator");
+const helmet     = require("helmet");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(validator());
 app.use(helmet());
 app.use("/", ROUTES);
 
