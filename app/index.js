89183474bb15f38aedcf5983193de9e07fd56bac
Alberto Silva <asilvadesigns@gmail.com>
2017-10-31 13:12:05 -0400

    cleanup listening log, install express validator

--- app/index.js
+++ app/index.js
@@ -4,11 +4,13 @@ const ROUTES = require("./routes");
 const app = require("express")();
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
+const validator = require("express-validator");
 
 app.use(bodyParser.json());
 app.use(helmet());
+app.use(validator());
 app.use("/", ROUTES);
 
 app.listen(CONFIG.port, CONFIG.host, () => {
-  console.log("Server listenting on port " + CONFIG.port + " ...");
+  console.log(`Server listenting at ${CONFIG.host}:${CONFIG.port}`);
 });
