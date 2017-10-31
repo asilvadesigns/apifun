7e16292bde42da8c7c5094b4b38479a2394dda9f
Alberto Silva <asilvadesigns@gmail.com>
2017-10-31 16:12:53 -0400

    extract sample data into file

---
+++ app/store/sampleData.js
@@ -0,0 +1,34 @@
+const _sampleData = {
+  measurements: [
+    {
+      "timestamp": "2015-09-01T16:00:00.000Z",
+      "temperature": 27.1,
+      "dewPoint": 16.9
+    },
+    {
+      "timestamp": "2015-09-01T16:10:00.000Z",
+      "temperature": 27.3
+    },
+    {
+      "timestamp": "2015-09-01T16:20:00.000Z",
+      "temperature": 27.5,
+      "dewPoint": 17.1
+    },
+    {
+      "timestamp": "2015-09-01T16:30:00.000Z",
+      "temperature": 27.4,
+      "dewPoint": 17.3
+    },
+    {
+      "timestamp": "2015-09-01T16:40:00.000Z",
+      "temperature": 27.2
+    },
+    {
+      "timestamp": "2015-09-01T17:00:00.000Z",
+      "temperature": 28.1,
+      "dewPoint": 18.3
+    }
+  ]
+};
+
+module.exports = _sampleData
